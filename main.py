b15ab29539111ab0fc76d41d5f36e20356b2c9a1
MrDionesalvi <dionealvip@gmail.com>
2024-03-21 16:52:06 +0100

    ciao

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-file_path = "input/00-trailer.txt"
+file_path = "/home/dione/Documenti/Università/Reply/main.py"
 
 with open(file_path, "r") as file:
-    content = file.read()
+    lines = file.readlines()
 
-print(content)
+# Extracting values from the first line
+values = lines[0].split()
+W = int(values[0])
+H = int(values[1])
+GN = int(values[2])
+SM = int(values[3])
+TL = int(values[4])
+
+print(f"W: {W}")
+print(f"H: {H}")
+print(f"GN: {GN}")
+print(f"SM: {SM}")
+print(f"TL: {TL}")
