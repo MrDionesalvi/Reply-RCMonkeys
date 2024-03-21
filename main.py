ba65c0206503159937304ac5c8f8837963f387fa
MrDionesalvi <dionealvip@gmail.com>
2024-03-21 17:04:20 +0100

    fix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-file_path = "/home/dione/Documenti/Università/Reply/main.py"
+file_path = "input/00-trailer.txt"
 
-with open(file_path, "r") as file:
+with open(file_path, "r", encoding='utf-8-sig') as file:
     lines = file.readlines()
 
 # Extracting values from the first line
 values = lines[0].split()
+print(values)
 W = int(values[0])
 H = int(values[1])
 GN = int(values[2])
