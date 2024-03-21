eac92e9c8c0f1c941ae09b953a522fc4e1db5ec0
MatteoCarriero <matteocarriero@proton.me>
2024-03-21 17:39:33 +0100

    gp_to_sp

--- main.py
+++ main.py
@@ -37,11 +37,20 @@ def distanza_sp(lista_sp, diz):
             col_distance = abs(lista_sp[i][0] - lista_sp[j][0])
             distance = row_distance + col_distance
             diz[f"{lista_sp[i][0]}_{lista_sp[i][1]}"].append([f"{lista_sp[j][0]}_{lista_sp[j][1]}", row_distance, col_distance, distance])
-    
             diz[f"{lista_sp[j][0]}_{lista_sp[j][1]}"].append([f"{lista_sp[i][0]}_{lista_sp[i][1]}", row_distance, col_distance, distance])
+
+def distanza_gp_to_sp(GPLIST, SPLIST, diz):
+    for gp in range(len(GPLIST)):
+        for sp in range(len(SPLIST)):
+            row_distance = abs(GPLIST[gp][1] - SPLIST[sp][1])
+            col_distance = abs(GPLIST[gp][0] - SPLIST[sp][0])
+            distance = row_distance + col_distance
+            diz[f"{GPLIST[gp][0]}_{GPLIST[gp][1]}"].append([f"{SPLIST[sp][0]}_{SPLIST[sp][1]}", row_distance, col_distance, disponibile])
+
 
 with open(file_path, "r", encoding='utf-8-sig') as file:
     line = file.readline()
+    gp_to_sp = {}
 
     # Extracting values from the first line
     values = line.split()
@@ -51,7 +60,7 @@ with open(file_path, "r", encoding='utf-8-sig') as file:
     SM = int(values[3])
     TL = int(values[4])
 
-    matrix = [[0 for _ in range(W)] for _ in range(H)]
+    matrix = [[0 for _ in range(W+1)] for _ in range(H+1)]
 
     lista_gp = []
     distanze_gp = {}
@@ -61,16 +70,16 @@ with open(file_path, "r", encoding='utf-8-sig') as file:
         matrix[i][j] = "G"
         lista_gp.append([i, j])
         distanze_gp[f"{i}_{j}"] = []
+        gp_to_sp[f"{i}_{j}"] = []
 
     distanza_gp(lista_gp, distanze_gp)
-    print(distanze_gp)
 
     silver_points = {}
     distanze_sp = {}
     lista_sp = []
     for _ in range(SM):
         line = file.readline().split(" ")
-        i, j, points = int(line[0])-1, int(line[1])-1, int(line[2]) 
+        i, j, points = int(line[0]), int(line[1]), int(line[2]) 
         silver_points[f"{i}_{j}"] = points
         matrix[i][j] = "S"
         lista_sp.append([i, j])
@@ -89,5 +98,7 @@ with open(file_path, "r", encoding='utf-8-sig') as file:
         costo_blocchi[i] = costo
         disponibilita_blocchi[i] = disponibile
 
+    distanza_gp_to_sp(lista_gp, lista_sp, gp_to_sp)
 
-        
+print(gp_to_sp)
+    
