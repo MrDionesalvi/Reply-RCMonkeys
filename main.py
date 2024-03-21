d7adf1bfaa857f4e8be31f9b183151b8da09e7a8
MatteoCarriero <matteocarriero@proton.me>
2024-03-21 16:21:05 +0100

    GOLDEN

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 file_path = "input/00-trailer.txt"
 
 with open(file_path, "r", encoding='utf-8-sig') as file:
-    lines = file.readlines()
+    line = file.readline()
 
-# Extracting values from the first line
-values = lines[0].split()
-print(values)
-W = int(values[0])
-H = int(values[1])
-GN = int(values[2])
-SM = int(values[3])
-TL = int(values[4])
+    # Extracting values from the first line
+    values = line.split()
+    W = int(values[0])
+    H = int(values[1])
+    GN = int(values[2])
+    SM = int(values[3])
+    TL = int(values[4])
 
-print(f"W: {W}")
-print(f"H: {H}")
-print(f"GN: {GN}")
-print(f"SM: {SM}")
-print(f"TL: {TL}")
+    matrix = [[0 for _ in range(W)] for _ in range(H)]
 
-matrix = [[0 for _ in range(W)] for _ in range(H)]
+
+    for _ in range(GN):
+        line = file.readline().split(" ")
+        i, j = int(line[0])-1, int(line[1])-1
+        matrix[i][j] = "G"
+
